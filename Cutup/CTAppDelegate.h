fedd3af684e8c58583d802296f2efc404d9add8b
James Shepherdson <james.shepherdson@gmail.com>
2012-05-20 16:26:48 -0400

    Initial Commit

---
+++ Cutup/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  Cutup
+//
+//  Created by James Shepherdson on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Cutup/CTAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  CTAppDelegate.m
+//  Cutup
+//
+//  Created by James Shepherdson on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CTAppDelegate.h"
+
+@implementation CTAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
